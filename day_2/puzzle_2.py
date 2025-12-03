7c7c6f46f42d3a0f1c8c9d7fbdd5707f0ad76ea8
Douglas Paxton <douglas_paxton@oxy.com>
2025-12-03 06:22:35 -0700

    day_3

--- day_2/puzzle_2.py
+++ day_2/puzzle_2.py
@@ -1,26 +1,23 @@
-import textwrap
 from time import time
 
 def solve(input):
     with open(input) as file:
         input = [(int(y[0]), int(y[1])) for y in [x.split('-') for x in file.read().split(',')]]
-    s = set()
+    s = 0
     for r in input:
         for n in range(r[0], r[1] + 1):
-            if n in s:
-                continue
             st = str(n)
             l = len(st)
-            for i in range(l - 1, 0, -1):
-                if int(l) % i != 0:
+            for m in range(1, l // 2 + 1):
+                if l % m != 0:
                     continue
-                lst = list(map(int, textwrap.wrap(st, i)))
-                if len(set(lst)) == 1:
-                    s.add(n)
+                b = st[:m]
+                if b * (l // m) == st:
+                    s += n
                     break
-    return sum(s)
+    return s
 if __name__ == '__main__':
-  input = 'day_2/input.txt'
-  t0 = time()
-  print('answer =', solve(input)) # 25912654282
-  print('runtime =', time() - t0, 's')
+    input = 'day_2/input.txt'
+    t0 = time()
+    print('answer =', solve(input)) # 25912654282
+    print('runtime =', time() - t0, 's')
---
+++ day_3/puzzle_2.py
@@ -0,0 +1,27 @@
+from time import time
+
+def solve(input):
+    with open(input) as file:
+        num_batts = 12
+        total = 0
+        for line in file.read().split():
+            bank = list(map(int, list(line)))
+            batts = [] 
+            end_idx = -(num_batts - 1)
+            for i in range(0, num_batts):
+                if end_idx == 0:
+                    maximum = max(bank)
+                else:
+                    maximum = max(bank[:end_idx])
+                batts.append(maximum)
+                idx = bank.index(maximum)
+                bank = bank[idx + 1:]
+                end_idx += 1
+            jolts = ''.join(list(map(str, batts)))
+            total += int(jolts)
+    return total
+if __name__ == '__main__':
+    input = 'day_3/input.txt'
+    t0 = time()
+    print('answer =', solve(input)) # 170418192256861
+    print('runtime =', time() - t0, 's')
