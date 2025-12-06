a29d045c3b0056ce6ec84fdea977ba92bb727cc7
Douglas Paxton <douglas_paxton@oxy.com>
2025-12-06 08:07:41 -0700

    day_6

---
+++ day_6/puzzle_2.py
@@ -0,0 +1,45 @@
+from time import time
+import copy
+from itertools import zip_longest
+
+def solve(input):
+    with open(input) as file:
+        input = [list(x) for x in file.read().split('\n')]
+        rows = len(input)
+        cols = len(input[0])
+        idxs = []
+        for c in range(cols):
+            col = []
+            for r in range(rows):
+                col.append(input[r][c])
+            if all(x == ' ' for x in col):
+                idxs.append(c)
+        
+        clean_input = []
+        for r in input:
+            copy_idxs = [-1] + copy.deepcopy(idxs) + [len(r)]
+            clean_input.append([r[copy_idxs[i] + 1:copy_idxs[i+1]] for i in range(len(copy_idxs)-1)])
+        for c in clean_input[-1]:
+            c[:] = c[0] * len(c)
+            
+        rows = len(clean_input)
+        cols = len(clean_input[0])
+        total = 0
+        for c in range(cols):
+            cephalopod_nums  = []
+            for r in range(rows - 1):
+                cephalopod_nums.append(clean_input[r][c])
+                op = clean_input[rows - 1][c][0]
+            human_nums = [''.join(chars) for chars in zip(*cephalopod_nums)]
+            equation = ''
+            for num in human_nums:
+                equation += num.replace(' ', '') + op
+            total += eval(equation[:-1])
+
+    return total
+
+if __name__ == '__main__':
+    input = 'day_6/input.txt'
+    t0 = time()
+    print('answer =', solve(input)) # 8907730960817
+    print('runtime =', round(time() - t0, 3), 's') # 0.055 s
